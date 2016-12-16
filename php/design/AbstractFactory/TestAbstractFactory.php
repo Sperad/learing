c2b59391aa56fb5ca00dd5b12688edbaccd298cc
jpher <sperad.com@gmail.com>
2016-12-16 16:23:18 +0800

    phpunit.xml

--- php/design/AbstractFactory/TestAbstractFactory.php
+++ php/design/AbstractFactory/TestAbstractFactory.php
@@ -3,9 +3,12 @@ namespace design\AbstractFactory;
 
 class TestAbstractFactory extends \PHPUnit_Framework_TestCase
 {
-    $factory = new HtmlFactory();
-    $content = 'htmlContent';
-    $text = $factory->createText($content);
-    $this->assertInstanceOf('design\AbstractFactory\HtmlText', $text);
-    $this->assertEquals($content, $text->getText());
+    public function testHtmlText()
+    {
+        $factory = new HtmlFactory();
+        $content = 'htmlContent';
+        $text = $factory->createText($content);
+        $this->assertInstanceOf('design\AbstractFactory\HtmlText', $text);
+        $this->assertEquals($content, $text->getText());
+    }
 }
